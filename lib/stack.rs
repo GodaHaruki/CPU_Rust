f029a153976fdda2b7f595fa8a0ff29ec5a536ab
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2022-11-17 15:33:04 +0900

    Update stack.rs

--- lib/stack.rs
+++ lib/stack.rs
@@ -61,5 +61,5 @@ mod tests{
                 value: "World",
                 node : None,
             }));
-        assert_eq!(format!("{}, {}" ,stack.at(&0).unwrap(), stack.at(&1).unwrap()), "Hello, World);
+        assert_eq!(format!("{}, {}" ,stack.at(&0).unwrap(), stack.at(&1).unwrap()), "Hello, World");
     }
