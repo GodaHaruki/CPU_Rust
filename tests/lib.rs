4e79c36388aedeef938fe4c526f3721928bd31f4
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2022-11-17 15:16:00 +0900

    Update lib.rs

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,15 +1,17 @@
-use memory::*;
-
-#[test]
-fn test_stack(){
-    let stack = stack::Stack::<&str> {
-        value: "Hello",
-        node: Some(Box::new(stack::Stack {
-            value: "World",
-            node: None,
-        })),
-    };
-    println!("{:#?}", stack);
-  
-    println!("{}, {}", stack.at(&0).unwrap(), stack.at(&1).unwrap());
+#[cfg(test)]
+pub mod tests {
+    use memory::*;
+    
+    #[test]
+    fn test_stack(){
+        let stack = stack::Stack::<&str> {
+            value: "Hello",
+            node: Some(Box::new(stack::Stack {
+                value: "World",
+                node: None,
+            })),
+        };
+        
+        assert_eq!(format!("{}, {}", stack.at(&0).unwrap(), stack.at(&1).unwrap()), "Hello, World");
+    }
 }
