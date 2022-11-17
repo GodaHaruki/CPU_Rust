15525989109d6e216fc2e73b27656ac1b2d088cb
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2022-11-17 14:45:54 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 use memory::stack;
 
-fn main(){
-  println!("Hello, world");
+fn main() {
+    let stack = Stack::<&str> {
+        value: "Hello",
+        child: Box::new(Some(Stack {
+            value: "World",
+            child: Box::new(None),
+        })),
+    };
+    println!("{:#?}", stack);
+  
+    println!("{}, {}", stack.at(0), stack.at(1));
 }
